41f1525986c6660cfa6b0fa24dc5dad096777147
rmit-s3740446-Ryan-Cassidy <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-01-29 19:10:21 +1100

    fixed validation for billcreate page

---
+++ BankingApplication/Attributes/ValidDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingApplication.Attributes
+{
+    public class ValidDate : ValidationAttribute
+    {
+        //Date validation referencing https://stackoverflow.com/questions/8844747/restrict-datetime-value-with-data-annotations
+        public override bool IsValid(object value)
+        {
+            DateTime d = Convert.ToDateTime(value);
+            return d >= DateTime.UtcNow;
+        }
+    }
+}
