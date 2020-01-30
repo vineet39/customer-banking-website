c550e8a723ce27418ffef97363002b58a971a02a
rmit-s3740446-Ryan-Cassidy <37936511+rmit-s3740446-Ryan-Cassidy@users.noreply.github.com>
2020-01-30 22:02:27 +1100

    Add files via upload
    Added Repository class library DLL and solution.

---
+++ RepositoryDLL/Repository/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public abstract class Repository<T> where T : class
+    {
+        private DbContext _context;
+        public Repository(DbContext context) => _context = context;
+        public IQueryable<T> GetByID(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _context.Set<T>();
+        }
+
+        public void Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
